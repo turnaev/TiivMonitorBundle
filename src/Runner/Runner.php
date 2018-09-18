c7d2eb723bcccd965869ac9851f95e3fde46cc88
Vladimir Turnaev <turnaev@gmail.com>
2018-09-18 16:52:30 +0700

    ~

--- src/Runner/Runner.php
+++ src/Runner/Runner.php
@@ -3,12 +3,8 @@
 namespace Tvi\MonitorBundle\Runner;
 
 use Tvi\MonitorBundle\Exception\NotFoundResourceException;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
-use ZendDiagnostics\Runner\Reporter\ReporterInterface;
 
 class Runner extends \ZendDiagnostics\Runner\Runner
 {
-
-
 
 }
---
+++ src/Runner/RunnerManager.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tvi\MonitorBundle\Runner;
+
+class RunnerManager
+{
+
+}
